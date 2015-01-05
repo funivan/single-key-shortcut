5428d0028350c8b7fefaf36705bb93730773f78c
Ivan Scherbak <dev@funivan.com>
2015-01-05 10:01:50 +0200

    Detect device id by regexp

--- run.sh
+++ run.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
  DEVICE=/dev/input/by-id/usb-Dell_Dell_USB_Entry_Keyboard-event-kbd
- ID=12
+ REGEXP="Dell"
+
+ ID=`xinput  | grep "$REGEXP" |  grep -oP "id=\d+" | grep -oP "\d+"`
  
+ if [ -z "$ID" ] ; then
+   echo "Can\`t detect device id";
+   exit 1;
+ fi
+
+  
  sudo chown ivan $DEVICE
- xinput disable $ID
+ xinput disable "$ID"
  make
   ./out/program -d $DEVICE -c keyboard.conf
-
- 
+  
