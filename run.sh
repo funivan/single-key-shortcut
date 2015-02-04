dc4046523cd161fafc9298ed487a109078670462
Ivan Scherbak <dev@funivan.com>
2015-02-04 17:24:46 +0200

    Remove chown

--- run.sh
+++ run.sh
@@ -3,15 +3,13 @@
  DEVICE=/dev/input/by-id/usb-Dell_Dell_USB_Entry_Keyboard-event-kbd
  REGEXP="Dell"
 
- ID=`xinput  | grep "$REGEXP" |  grep -oP "id=\d+" | grep -oP "\d+"`
+ ID=`xinput | grep "$REGEXP" |  grep -oP "id=\d+" | grep -oP "\d+"`
  
  if [ -z "$ID" ] ; then
    echo "Can\`t detect device id";
    exit 1;
  fi
 
-  
- sudo chown ivan $DEVICE
  xinput disable "$ID"
  make
   ./out/program -d $DEVICE -c keyboard.conf
